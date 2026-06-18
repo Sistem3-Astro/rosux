2bc5b0df8366cae9f0b11d3462775d56b733d242
SMX-SISTEMAS <asistente.sistemas2@opcionessacimex.com>
2026-06-17 18:23:57 -0600

    Hi

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
@@ -8,6 +7,7 @@ import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+ 
 
   return (
     <Tabs
@@ -16,20 +16,37 @@ export default function TabLayout() {
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    
+    <Tabs.Screen
+    name="formu"
+    options={{
+      title: "Formulario",
+      headerShown: true,
+      headerTitle: "Formulario",
+      headerStyle: {
+      backgroundColor: "#0D6337",
+    },
+    headerTintColor: "#fff",
+      tabBarIcon: ({ color }) => (
+        <IconSymbol size={28} name="doc.text.fill" color={color} />
+      ),
+    }}
+  />
+   <Tabs.Screen
+    name="explore"
+    options={{
+      title: "Explore",
+      headerShown: true,
+      headerTitle: "Visualizar",
+      headerStyle: {
+      backgroundColor: "#0D6337",
+    },
+    headerTintColor: "#fff",
+      tabBarIcon: ({ color }) => (
+        <IconSymbol size={28} name="doc.text.fill" color={color} />
+      ),
+    }}
+  />
     </Tabs>
   );
 }
